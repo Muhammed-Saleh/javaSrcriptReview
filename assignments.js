e469a3fe4892b02ba333b795acf2567e97baff71
Muhammed-Saleh <hamada500005@gmail.com>
2022-02-11 16:45:45 -0800

    Assignment of LECTURE: Equality Operators: == vs. ===

--- assignments.js
+++ assignments.js
@@ -1,19 +1,28 @@
-let marksWeightInKg = 78;
-let jhonsWeightInKg = 92;
-let marksHeightInM = 1.69;
-let jhonsHeightInM = 1.95;
+// let marksWeightInKg = 78;
+// let jhonsWeightInKg = 92;
+// let marksHeightInM = 1.69;
+// let jhonsHeightInM = 1.95;
 
 // let marksWeightInKg = 95;
 // let jhonsWeightInKg = 85;
 // let marksHeightInM = 1.88;
 // let jhonsHeightInM = 1.76;
 
-const marksBMI = marksWeightInKg / marksHeightInM ** 2;
-const jhonsBMI = jhonsWeightInKg / (jhonsHeightInM * jhonsHeightInM);
+// const marksBMI = marksWeightInKg / marksHeightInM ** 2;
+// const jhonsBMI = jhonsWeightInKg / (jhonsHeightInM * jhonsHeightInM);
 
 
-if (marksBMI > jhonsBMI) {
-    console.log(`Mark's BMI (${marksBMI}) is higher than Jhon's (${jhonsBMI})`);
+// if (marksBMI > jhonsBMI) {
+//     console.log(`Mark's BMI (${marksBMI}) is higher than Jhon's (${jhonsBMI})`);
+// } else {
+//     console.log(` Jhon's BMI (${jhonsBMI}) is higher than Mark's (${marksBMI})`);
+// }
+
+let numNeighbours = prompt('How many neighbour countries does your country have ? ');
+if (numNeighbours === 1) {
+    console.log(`Only 1 border!`);
+} else if (numNeighbours > 1) {
+    console.log(`More than 1 border`);
 } else {
-    console.log(` Jhon's BMI (${jhonsBMI}) is higher than Mark's (${marksBMI})`);
+    console.log(`No borders`);
 }
