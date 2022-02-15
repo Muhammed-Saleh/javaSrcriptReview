8e25ae0dc727ac043f66e1078305846f4e3c62df
Muhammed-Saleh <hamada500005@gmail.com>
2022-02-14 20:11:51 -0800

    Coding Challenge #6

--- assignments.js
+++ assignments.js
@@ -116,25 +116,62 @@
 // Apply this to the team's average scores �
 // GOOD LUCK
 
-let calcAverage = (firstScore, secondScore, thirdScore) => (firstScore + secondScore + thirdScore) / 3;
+// let calcAverage = (firstScore, secondScore, thirdScore) => (firstScore + secondScore + thirdScore) / 3;
 
-let avgDolhins01 = calcAverage(44, 23, 71);
-let avgKoalas01 = calcAverage(65, 54, 49);
+// let avgDolhins01 = calcAverage(44, 23, 71);
+// let avgKoalas01 = calcAverage(65, 54, 49);
 
-let avgDolhins02 = calcAverage(85, 54, 41);
-let avgKoalas02 = calcAverage(23, 34, 27);
+// let avgDolhins02 = calcAverage(85, 54, 41);
+// let avgKoalas02 = calcAverage(23, 34, 27);
 
-function checkWinner(avgDolhins, avgKoalas) {
-    if (avgDolhins >= 2 * avgKoalas) {
-        console.log(`Dolphins win (${avgDolhins} vs. ${avgKoalas})`);
-    } else if (avgKoalas >= 2 * avgDolhins) {
-        console.log(`Koalas win (${avgKoalas} vs. ${avgDolhins})`);
-    } else {
-        console.log(`no team wins!`);
-    }
+// function checkWinner(avgDolhins, avgKoalas) {
+//     if (avgDolhins >= 2 * avgKoalas) {
+//         console.log(`Dolphins win (${avgDolhins} vs. ${avgKoalas})`);
+//     } else if (avgKoalas >= 2 * avgDolhins) {
+//         console.log(`Koalas win (${avgKoalas} vs. ${avgDolhins})`);
+//     } else {
+//         console.log(`no team wins!`);
+//     }
+// }
+
+// checkWinner(avgDolhins01, avgKoalas01);
+// checkWinner(avgDolhins02, avgKoalas02);
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Coding Challenge #2
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+// the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+// 20%.
+// Your tasks:
+// 1. Write a function 'calcTip' that takes any bill value as an input and returns
+// the corresponding tip, calculated based on the rules above (you can check out
+// the code from first tip calculator challenge if you need to). Use the function
+// type you like the most. Test the function using a bill value of 100
+// 2. And now let's use arrays! So create an array 'bills' containing the test data
+// below
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from
+// the function you created before
+// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+// Test data: 125, 555 and 44
+// Hint: Remember that an array needs a value in each position, and that value can
+// actually be the returned value of a function! So you can just call a function as array
+// values (so don't store the tip values in separate variables first, but right in the new
+// array) �
+// GOOD LUCK 
+
+
+function calcTip(bill) {
+    let precent = 300 >= bill && bill >= 50 ? 15 / 100 : 20 / 100;
+    return (bill * precent);
 }
 
-checkWinner(avgDolhins01, avgKoalas01);
-checkWinner(avgDolhins02, avgKoalas02);
+let bills = [calcTip(125), calcTip(555), calcTip(44)];
+console.log(bills);
 
+let total = [calcTip(125) + 125, calcTip(555) + 555, calcTip(44) + 44];
+console.log(total);
 
